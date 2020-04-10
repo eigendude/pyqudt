fc9962a7f70ffea8dedc5f164c3125447b599513
Garrett Brown <garrett.brown@aclima.io>
2020-04-10 00:09:27 -0700

    Add PartsPerMillion unit from Open PHACTS ontology

---
+++ qudt/ontology/ops.py
@@ -0,0 +1,23 @@
+################################################################################
+#
+#  Copyright (C) 2019 Garrett Brown
+#  This file is part of pyqudt - https://github.com/eigendude/pyqudt
+#
+#  pyqudt is derived from jQUDT
+#  Copyright (C) 2012-2013  Egon Willighagen <egonw@users.sf.net>
+#
+#  SPDX-License-Identifier: BSD-3-Clause
+#  See the file LICENSE for more information.
+################################################################################
+
+from qudt.ontology.ontology_utils import OntologyUtils
+
+
+OntologyUtils.register_namespace('ops', 'http://www.openphacts.org/units/')
+
+
+class OPS(object):
+    """
+    Open PHACTS ontology.
+    """
+    namespace = OntologyUtils.get_namespace('ops')
---
+++ qudt/units/quantity_kind.py
@@ -0,0 +1,21 @@
+################################################################################
+#
+#  Copyright (C) 2020 Garrett Brown
+#  This file is part of pyqudt - https://github.com/eigendude/pyqudt
+#
+#  pyqudt is derived from jQUDT
+#  Copyright (C) 2012-2013  Egon Willighagen <egonw@users.sf.net>
+#
+#  SPDX-License-Identifier: BSD-3-Clause
+#  See the file LICENSE for more information.
+#
+################################################################################
+
+from qudt.unit import Unit
+from qudt.ontology.unit_factory import UnitFactory
+
+
+class QuantityKind(object):
+    """
+    """
+    PARTS_PER_MILLION: Unit = UnitFactory.get_unit('http://www.openphacts.org/units/PartsPerMillion')
